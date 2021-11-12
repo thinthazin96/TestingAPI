f6e746792de89c5dbd1b25e8c64846ab5478ab29
Thin Thazin <thinthazin@imac.myfiosgateway.com>
2021-11-12 17:58:49 -0500

    Adding header

--- basic_API.js
+++ basic_API.js
@@ -1,6 +1,9 @@
 fetch('https://reqres.in/api/users', 
     {
     method: 'POST', //updating exisiting database
+    headers:{
+        'Content-Type': 'application/json'
+    }, //won't show user's name without header
     body: JSON.stringify({ //must pass as JSON.stringify format to add data in fetch
         name: 'User 1' //adding new user
     })
